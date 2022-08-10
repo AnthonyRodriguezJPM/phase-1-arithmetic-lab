7784769eb9ce4b365b51b3680d51f02e30926588
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-10 11:20:29 -0700

    commit tree 55dfb4b5e810353803542c12944113d3a22ac3a5

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1*num2;
+console.log(multiply);
+//////
+const random=Math.floor(Math.random()*10)+1;
+console.log(random);
+//////
+const num3=19;
+const num4=5;
+
+const mod=num3%num4;
+console.log(mod);
+/////
+
+const max=(1,2,4,5,6,7,8,9,10,11,15,20);
+console.log(Math.max(max));
